49df3f9c14093c88c49fcc587ee94ebbca294f85
RominaMaita <u202213765@upc.edu.pe>
2025-05-06 18:21:56 -0500

    feat(icons): added the icons for health monitoring form.

---
+++ src/assets/IconOxygen.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M12.5 3a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-3 2a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm-6-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-3 2a3 3 0 1 1 6 0 3 3 0 0 1-6 0z"/>
+    <path d="M5.5 10.5A3.5 3.5 0 0 1 2 7h1a2.5 2.5 0 0 0 5 0h1a3.5 3.5 0 0 1-3.5 3.5zM10 5a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+  </svg>
+</template>
