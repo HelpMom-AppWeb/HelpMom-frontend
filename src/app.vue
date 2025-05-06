1451789bf350b7463d44cba177bab5a8fc7c3785
RominaMaita <u202213765@upc.edu.pe>
2025-05-06 18:24:43 -0500

    feat(main): add health monitoring component in app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,34 @@
+<template>
+  <div id="app">
+    <!-- Language switcher visible siempre -->
+
+
+    <!-- Puedes mover esto según tu diseño general -->
+    <HealthMonitoring />
+  </div>
+</template>
+
 <script>
-import LanguageSwitcher from "./public/components/language-switcher.component.vue";
+//import LanguageSwitcher from "./public/components/language-switcher.component.vue";
+import HealthMonitoring from "./health-monitoring/pages/health-monitoring.component.vue" // Ajusta el path si es necesario
+
 export default {
   name: 'app',
-  components: {LanguageSwitcher},
+  components: {
+    HealthMonitoring
+  },
   data() {
     return {
       drawer: false,
       items: [
         { label: 'option.home', to: '/home' }
       ]
-    }
+    };
   },
   methods: {
     toggleDrawer() {
       this.drawer = !this.drawer;
     }
   }
-}
+};
 </script>
