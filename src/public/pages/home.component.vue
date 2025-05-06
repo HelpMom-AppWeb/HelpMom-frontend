f28520e9e07fee7a76c08b57d9a28254ec786178
Ro <u202213765@upc.edu.pe>
2025-05-05 23:39:28 -0500

    feat(home): add home component.

---
+++ src/public/pages/home.component.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "home.component"
+}
+</script>
+
+<template>
+  <div class="w-full">
+    <h1> Welcome</h1>
+    <p>Welcome to HelpMom</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
