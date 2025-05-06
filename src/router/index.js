8504cfe34640b4a4b6af747cdc4afcd127dbcfd5
RominaMaita <u202213765@upc.edu.pe>
2025-05-06 18:22:49 -0500

    feat(index): import Health monitoring component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import HomeComponent from "../public/pages/home.component.vue";
 import {createRouter, createWebHistory} from "vue-router";
 
-const AboutComponent = () => import('../public/pages/about.component.vue');
 const PageNotFoundComponent = () => import('../public/pages/page-not-found.component.vue');
+const HealthMonitoringComponent = () => import('../health-monitoring/pages/health-monitoring.component.vue')
+
 const routes = [
     { path: '/home',            name: 'home',       component: HomeComponent,           meta: { title: 'Home' } },
-    { path: '/about',           name: 'about',      component: AboutComponent,          meta: { title: 'About us' } },
     { path: '/',                name: 'default',    redirect: '/home'  },
     { path: '/:pathMatch(.*)*', name: 'not-found',  component: PageNotFoundComponent,   meta: { title: 'Page not found' } },
 ];
@@ -17,7 +17,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
     console.log(`Navigating from ${from.name} to ${to.name}`);
-    let baseTitle = 'ACME Learning Center';
+    let baseTitle = 'HelpMom';
     document.title = `${baseTitle} | ${to.meta['title']}`;
     next();
 });
