4b7d6892a77b5d1d83cfad6d613b892ab810aa10
Ro <u202213765@upc.edu.pe>
2025-05-05 23:43:45 -0500

    feat(main): add basic configuration.

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<script>
+import LanguageSwitcher from "./public/components/language-switcher.component.vue";
+export default {
+  name: 'app',
+  components: {LanguageSwitcher},
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { label: 'option.home', to: '/home' }
+      ]
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    }
+  }
+}
+</script>
