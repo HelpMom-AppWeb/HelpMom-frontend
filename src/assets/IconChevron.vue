9f1cfe72576923ffefecb7e0326653d687eba018
RominaMaita <u202213765@upc.edu.pe>
2025-05-07 17:26:26 -0500

    feat: add personalized icons for the sidebar.

---
+++ src/assets/IconChevron.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      viewBox="0 0 16 16"
+      :style="transformStyle"
+  >
+    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+  </svg>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  direction: {
+    type: String,
+    default: 'left',
+    validator: (value) => ['up', 'right', 'down', 'left'].includes(value)
+  }
+});
+
+const transformStyle = computed(() => {
+  const rotations = {
+    up: 'rotate(90deg)',
+    right: 'rotate(180deg)',
+    down: 'rotate(270deg)',
+    left: 'rotate(0deg)'
+  };
+
+  return { transform: rotations[props.direction] };
+});
+</script>
---
+++ src/assets/IconDashboard.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H4z"/>
+  </svg>
+</template>
