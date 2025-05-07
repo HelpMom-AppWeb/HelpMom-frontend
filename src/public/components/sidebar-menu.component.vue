5abb33c6de039974a425e2d06593402b98810f47
RominaMaita <u202213765@upc.edu.pe>
2025-05-07 18:02:54 -0500

    fix: changed sidebar component.

--- src/public/components/sidebar-menu.component.vue
+++ src/public/components/sidebar-menu.component.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sidebar-container">
-    <Button icon="pi pi-bars" @click="visible = true" class="menu-button" />
+
 
     <Sidebar v-model:visible="visible" position="left" class="custom-sidebar">
       <div class="profile-section">
@@ -13,40 +13,40 @@
           <IconDashboard class="mr-2" /> Dashboard
         </router-link>
         <router-link to="/profile" class="menu-item" :class="{ active: isActive('/profile') }">
-          <i class="pi pi-user mr-2"></i> Profile
+          <IconProfile class="mr-2" /> Profile
         </router-link>
       </div>
 
       <div class="menu-group">
         <p class="section-title">ANALYTICS</p>
         <router-link to="/health" class="menu-item" :class="{ active: isActive('/health') }">
-          <i class="pi pi-plus mr-2"></i> Health monitoring
+          <IconHealth class="mr-2" /> Health Monitoring
         </router-link>
       </div>
 
       <div class="menu-group">
         <p class="section-title">SCHEDULE</p>
         <router-link to="/chat" class="menu-item" :class="{ active: isActive('/chat') }">
-          <i class="pi pi-comments mr-2"></i> Chat
+          <IconChat class="mr-2" /> Chat
         </router-link>
         <router-link to="/appointments" class="menu-item" :class="{ active: isActive('/appointments') }">
-          <i class="pi pi-calendar mr-2"></i> Appointments
+          <IconCalendar class="mr-2" /> Appointments
         </router-link>
       </div>
 
       <div class="menu-group">
         <p class="section-title">HELP</p>
         <router-link to="/notifications" class="menu-item" :class="{ active: isActive('/notifications') }">
-          <i class="pi pi-bell mr-2"></i> Notifications
+          <IconNotification class="mr-2" /> Notifications
         </router-link>
         <router-link to="/settings" class="menu-item" :class="{ active: isActive('/settings') }">
-          <i class="pi pi-cog mr-2"></i> Settings
+          <IconSettings class="mr-2" /> Settings
         </router-link>
       </div>
 
       <div class="logout">
         <a class="menu-item logout-btn" href="#">
-          <i class="pi pi-sign-out mr-2"></i> Log out
+          <IconLogout class="mr-2" /> LogOut
         </a>
       </div>
     </Sidebar>
@@ -55,10 +55,24 @@
 
 <script>
 import IconDashboard from "../../assets/IconDashboard.vue";
+import IconProfile from "../../assets/IconProfile.vue";
+import IconHealth from "../../assets/IconHealth.vue";
+import IconChat from "../../assets/IconChat.vue";
+import IconCalendar from "../../assets/IconCalendar.vue";
+import IconNotification from "../../assets/IconNotification.vue";
+import IconSettings from "../../assets/IconSettings.vue";
+import IconLogout from "../../assets/IconLogout.vue";
 
 export default {
   name: 'sidebar-menu',
   components: {
+    IconLogout,
+    IconSettings,
+    IconNotification,
+    IconCalendar,
+    IconChat,
+    IconHealth,
+    IconProfile,
     IconDashboard
   },
   data() {
@@ -83,9 +97,8 @@ export default {
   margin: 1rem;
 }
 
-.custom-sidebar {
-  width: 250px;
-  padding: 1rem;
+.custom-sidebar .p-sidebar {
+  width: 4000px; /* o cualquier ancho deseado */
 }
 
 .profile-section {
