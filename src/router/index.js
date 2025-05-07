2b648f7f03199d6ca151e9587f5239e767689104
RominaMaita <u202213765@upc.edu.pe>
2025-05-07 17:26:37 -0500

    feat: add route dashboard.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import HomeComponent from "../public/pages/home.component.vue";
 import {createRouter, createWebHistory} from "vue-router";
+import DashboardComponent from "../public/pages/dashboard.component.vue";
+
 
 const PageNotFoundComponent = () => import('../public/pages/page-not-found.component.vue');
 const routes = [
     { path: '/home',            name: 'home',       component: HomeComponent,           meta: { title: 'Home' } },
     { path: '/',                name: 'default',    redirect: '/home'  },
+    {path: '/dashboard',        name: 'dashboard', component: DashboardComponent, meta: { title: 'Dashboard' }},
     { path: '/:pathMatch(.*)*', name: 'not-found',  component: PageNotFoundComponent,   meta: { title: 'Page not found' } },
 ];
 
@@ -15,7 +18,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
     console.log(`Navigating from ${from.name} to ${to.name}`);
-    let baseTitle = 'ACME Learning Center';
+    let baseTitle = 'HelpMom';
     document.title = `${baseTitle} | ${to.meta['title']}`;
     next();
 });
