674b4be3d60fb179ad7db969f03c133f882af76e
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-05-11 16:15:00 -0500

    feat: added patient list component

---
+++ src/patient-management/components/patient-list.component.vue
@@ -0,0 +1,24 @@
+<script>
+import PatientItem from "./patient-item.component.vue";
+
+export default {
+  name: "patient-list",
+  components: {PatientItem},
+  props: {
+    patients: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div v-for="patient in patients" :key="patient.id">
+    <patient-item :patient="patient"></patient-item>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
