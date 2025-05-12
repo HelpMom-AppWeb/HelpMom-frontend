e3bf8e78879fb3d11a25920253f0770fc2530ca3
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-05-11 21:44:14 -0500

    feat: added add-patient component

---
+++ src/patient-management/pages/add-patient.component.vue
@@ -0,0 +1,40 @@
+<script>
+import InputText from "primevue/inputtext";
+import Button from "primevue/button"
+import { Form } from '@primevue/forms';
+import Message  from 'primevue/message'
+
+
+export default {
+  name: "add-patient",
+  components: {InputText, Button, Form}
+}
+</script>
+
+<template>
+  <div >
+    <p>a</p>
+    <Form>
+      <div class="flex flex-col gap-4 w-full h-10rem">
+        <div class="flex flex-col gap-1">
+          <label for="patientName">Personal Name</label>
+          <InputText name="patientName" type="text" />
+          <Message v-if="$form.patientName?.invalid" severity="error" size="small" variant="simple">{{ $form.patientName.error?.message }}</Message>
+        </div>
+
+        <div class="flex flex-col gap-1">
+          <label for="email">Email</label>
+          <InputText name="email" type="text" />
+          <Message v-if="$form.email?.invalid" severity="error" size="small" variant="simple">{{ $form.email.error?.message }}</Message>
+        </div>
+      </div>
+      <Button type="Save" severity="success" label="Save" />
+
+      <Button type="Cancel" severity="success" label="Cancel" />
+    </Form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
