9940792caf690ae55e02ad138b2a2e0f195acb68
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-05-11 21:43:03 -0500

    feat: fixed patients not displaying, added dummy data

--- src/patient-management/components/patient-list.component.vue
+++ src/patient-management/components/patient-list.component.vue
@@ -1,9 +1,10 @@
 <script>
 import PatientItem from "./patient-item.component.vue";
+import Button from "primevue/button";
 
 export default {
   name: "patient-list",
-  components: {PatientItem},
+  components: {Button, PatientItem},
   props: {
     patients: {
       type: Array,
@@ -14,8 +15,17 @@ export default {
 </script>
 
 <template>
-  <div v-for="patient in patients" :key="patient.id">
+  <div class="flex flex-column" v-for="patient in patients" :key="patient.id">
     <patient-item :patient="patient"></patient-item>
+  </div>
+
+  <div style="position: fixed; bottom: 50px; right: 90px;">
+    <router-link to="/about">
+      <Button label="Agregar paciente"
+              style="background-color: #BDFFB3; border-color: #000000; color: #000000;
+              width: 200px; height: 80px; border-radius: 20px"
+              icon="pi pi-plus" iconPos="right" />
+    </router-link>
   </div>
 </template>
 
