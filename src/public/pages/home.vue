f34ef47b5f66dc8f69baf1574b1af550bf414f1b
Ro <u202213765@upc.edu.pe>
2025-05-14 10:59:19 -0500

    feat: add fetal analytics to dashboard

--- src/public/pages/home.vue
+++ src/public/pages/home.vue
@@ -1,48 +1,44 @@
-<script>
-import {ref} from "vue";
+<script setup>
+import { ref } from 'vue';
 
-export default {
-  name: "home.component",
-  components: {},
+// Peso fetal
+const fetalWeightSeries = ref([{ name: "Estimated weight (g)", data: [1, 4, 20, 100, 300, 600, 1200, 1800, 2500, 3200] }]);
+const fetalWeightOptions = ref({
+  chart: { id: "fetal-growth-line" },
+  xaxis: { title: { text: "Week of gestation" }, categories: ["6", "8", "10", "12", "16", "20", "24", "28", "32", "36"] },
+  colors: ['#8c00ff'],
+  yaxis: { title: { text: "Weight (g)" } }
+});
 
-  setup() {
-    const lineChartSeries = ref([
-      {
-        name: "sales",
-        data: [31, 40, 28, 51, 42, 109, 100],
-      },
-    ]);
+// Frecuencia cardíaca
+const heartRateSeries = ref([{ name: "Heart Rate", data: [90, 120, 140, 150, 145, 140, 135] }]);
+const heartRateOptions = ref({
+  chart: { id: "fetal-heart-rate" },
+  xaxis: { title: { text: "Week of gestation" }, categories: ["6", "8", "10", "12", "14", "16", "18"] },
+  colors: ['#8c00ff'],
+  yaxis: { title: { text: "Beats per minute" } }
+});
 
-    const lineChartOptions = ref({
-      chart: {
-        id: "basic-line",
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997],
-      },
-    });
-
-    return {
-      lineChartSeries,
-      lineChartOptions,
-    };
-  },
-}
-
-
+// Estadísticas
+const fetalStatsSeries = ref([{ name: "Week 24", data: [1300, 150, 25] }]);
+const fetalStatsOptions = ref({
+  chart: { type: "radar" },
+  labels: ["Weight (g)", "Heart Rate (bpm)", "Lenght (cm)"]
+});
 </script>
+
+
 
 <template>
   <div>
-    <h2>
-      ApexChart Example
-    </h2>
+    <h2>Fetal development</h2>
+    <apexchart type="line" :options="fetalWeightOptions" :series="fetalWeightSeries" height="300" />
 
-    <apexchart width="500" height="300"
-        type="line"
-        :series="lineChartSeries"
-        :options="lineChartOptions"
-    />
+    <h2>Fetal heart rate</h2>
+    <apexchart type="line" :options="heartRateOptions" :series="heartRateSeries" height="300" />
+
+    <h2>Parameter Summary</h2>
+    <apexchart type="radar" :options="fetalStatsOptions" :series="fetalStatsSeries" height="300" />
   </div>
 </template>
 
