ff6df6f018bb23d5643e7e6b234bf94f1a9c5ce8
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-06-01 12:49:43 -0500

    fix: fixed add patient component

--- src/patient-management/pages/add-patient.component.vue
+++ src/patient-management/pages/add-patient.component.vue
@@ -1,37 +1,74 @@
-<script>
-import { Form } from '@primevue/forms';
-import Message  from 'primevue/message'
-
-export default {
-  name: "add-patient",
-  components: {Form, Message},
-}
-</script>
-
 <template>
-  <div>
-    <p>a</p>
-    <Form>
-      <div class="flex flex-col gap-4 w-full h-10rem">
-        <div class="flex flex-col gap-1">
-          <label for="patientName">Personal Name</label>
-          <pv-input-text name="patientName" type="text" />
-          <Message v-if="$form.patientName?.invalid" severity="error" size="small" variant="simple">{{ $form.patientName.error?.message }}</Message>
+  <div class="flex justify-content-center align-items-center min-h-screen">
+    <pv-card class="w-full md:w-8 lg:w-6 p-4">
+      <template #title>
+        <h2>Add Patient's Personal Details</h2>
+      </template>
+      <template #content>
+        <div class="grid">
+          <div class="col-12 md:col-6">
+            <label for="personal-name" class="block text-900 font-medium mb-2">Personal Name</label>
+            <pv-input-text id="personal-name" type="text" class="w-full" v-model="form.nombre" placeholder="Lucia Camaro Valenzuela Ramirez"/>
+          </div>
+          <div class="col-12 md:col-6">
+            <label for="personal-email" class="block text-900 font-medium mb-2">Email</label>
+            <pv-input-text id="personal-email" type="email" class="w-full" v-model="form.email" placeholder="lcvr@hotmail.com"/>
+          </div>
+          <div class="col-12 md:col-6">
+            <label for="phone-number" class="block text-900 font-medium mb-2">Phone Number</label>
+            <pv-input-text id="phone-number" type="text" class="w-full" v-model="form.telefono" placeholder="555-1234" />
+          </div>
+          <div class="col-12 md:col-6">
+            <label for="baby-name" class="block text-900 font-medium mb-2">Baby's Name</label>
+            <pv-input-text id="baby-name" type="text" class="w-full" v-model="form.bebe" placeholder="Andres Cardenas Valenzuela"/>
+          </div>
+          <div class="col-12 md:col-6">
+            <label for="baby-birth-date" class="block text-900 font-medium mb-2">Baby's Birth Date</label>
+            <DatePicker id="baby-birth-date" class="w-full" v-model="form.nacimiento" placeholder="2025-05-01" />
+          </div>
+          <div class="col-12 md:col-6">
+            <label for="assigned-doctor" class="block text-900 font-medium mb-2">Assigned Doctor</label>
+            <pv-select id="assigned-doctor" class="w-full" :options="doctorOptions" v-model="form.doctor" placeholder="Maria Perez"/>
+          </div>
         </div>
-
-        <div class="flex flex-col gap-1">
-          <label for="email">Email</label>
-          <pv-input-text name="email" type="text" />
-          <Message v-if="$form.email?.invalid" severity="error" size="small" variant="simple">{{ $form.email.error?.message }}</Message>
+      </template>
+      <template #footer>
+        <div class="flex justify-content-end">
+          <router-link to="/patient-management/patients">
+            <pv-button label="Guardar" icon="pi pi-check" class="mr-2" @click.native="onSubmit" />
+          </router-link>
+          <router-link to="/patient-management/patients">
+            <pv-button label="Cancelar" icon="pi pi-times" class="p-button-secondary" />
+          </router-link>
         </div>
-      </div>
-      <pv-button type="Save" severity="success" label="Save" />
-
-      <pv-button type="Cancel" severity="success" label="Cancel" />
-    </Form>
+      </template>
+    </pv-card>
   </div>
 </template>
 
-<style scoped>
+<script setup>
+import DatePicker from 'primevue/datepicker';
+import { ref } from 'vue';
+import { useToast } from "primevue/usetoast";
+
+const toast = useToast();
+
+const onSubmit = () => {
+    toast.add({ severity: 'success', summary: 'Successful operation', detail: 'Patient data was saved', life: 3000 });
+};
+
+const form = ref({
+  nombre: "",
+  email: "",
+  telefono: "",
+  bebe: "",
+  nacimiento: "",
+  doctor: ""
+});
+const doctorOptions = ref(["Maria Perez", "Carlos Sánchez", "Ana Gutierrez"]);
+
+</script>
+
+<style>
 
 </style>
