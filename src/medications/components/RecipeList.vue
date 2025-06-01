d9f0d46186c4cc652e9520fdcf4f94f290e8b76b
Stephano <Stephanoescu@gmail.com>
2025-06-01 12:14:06 -0500

    feat: add RecipeList.

--- src/medications/components/RecipeList.vue
+++ src/medications/components/RecipeList.vue
@@ -1,37 +1,51 @@
 <script setup>
-import RecipeCard from './RecipeCard.vue';
+  import RecipeCard from './RecipeCard.vue';
 
-defineProps({
-  recipes: {
-    type: Array,
-    required: true
+  defineProps({
+    recipes: {
+      type: Array,
+      required: true
+    }
+  });
+
+  const emit = defineEmits(['add-medication', 'add-note', 'delete-medication', 'delete-recipe']);
+
+  const handleAddMedication = (recipeId) => {
+    emit('add-medication', recipeId);
+  };
+
+  const handleAddNote = (recipeId, noteText) => {
+    emit('add-note', recipeId, noteText);
+  };
+
+  const handleDeleteMedication = (recipeId, medicationIndex) => {
+    emit('delete-medication', recipeId, medicationIndex);
+  };
+
+  const handleDeleteRecipe = (recipeId) => {
+    emit('delete-recipe', recipeId);
+  };
+  </script>
+
+  <template>
+    <div class="recipe-list">
+      <RecipeCard
+        v-for="recipe in recipes"
+        :key="recipe.id"
+        :recipe="recipe"
+        @add-medication="handleAddMedication"
+        @add-note="handleAddNote"
+        @delete-medication="handleDeleteMedication"
+        @delete-recipe="handleDeleteRecipe"
+      />
+    </div>
+  </template>
+
+  <style scoped>
+  .recipe-list {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    margin-bottom: 20px;
   }
-});
-
-const emit = defineEmits(['add-medication', 'add-note', 'delete-medication']);
-
-const handleAddMedication = (recipeId) => {
-  emit('add-medication', recipeId);
-};
-
-const handleAddNote = (recipeId, noteText) => {
-  emit('add-note', recipeId, noteText);
-};
-
-const handleDeleteMedication = (recipeId, medicationIndex) => {
-  emit('delete-medication', recipeId, medicationIndex);
-};
-</script>
-
-<template>
-  <div class="recipe-list">
-    <RecipeCard
-      v-for="recipe in recipes"
-      :key="recipe.id"
-      :recipe="recipe"
-      @add-medication="handleAddMedication"
-      @add-note="handleAddNote"
-      @delete-medication="handleDeleteMedication"
-    />
-  </div>
-</template>
+  </style>
