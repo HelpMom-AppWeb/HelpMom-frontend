745640410915ee70c9077bdf2669d9ec6ef021f0
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-07-05 16:29:20 -0500

    fix: fixed main components

--- src/patient-management/pages/add-patient.component.vue
+++ src/patient-management/pages/add-patient.component.vue
@@ -7,27 +7,27 @@
       <template #content>
         <div class="grid">
           <div class="col-12 md:col-6">
-            <label for="personal-name" class="block text-900 font-medium mb-2">Personal Name</label>
+            <label for="personal-name" class="block font-medium mb-2">Personal Name</label>
             <pv-input-text id="personal-name" type="text" class="w-full" v-model="form.nombre" placeholder="Lucia Camaro Valenzuela Ramirez"/>
           </div>
           <div class="col-12 md:col-6">
-            <label for="personal-email" class="block text-900 font-medium mb-2">Email</label>
+            <label for="personal-email" class="block font-medium mb-2">Email</label>
             <pv-input-text id="personal-email" type="email" class="w-full" v-model="form.email" placeholder="lcvr@hotmail.com"/>
           </div>
           <div class="col-12 md:col-6">
-            <label for="phone-number" class="block text-900 font-medium mb-2">Phone Number</label>
+            <label for="phone-number" class="block font-medium mb-2">Phone Number</label>
             <pv-input-text id="phone-number" type="text" class="w-full" v-model="form.telefono" placeholder="555-1234" />
           </div>
           <div class="col-12 md:col-6">
-            <label for="baby-name" class="block text-900 font-medium mb-2">Baby's Name</label>
+            <label for="baby-name" class="block font-medium mb-2">Baby's Name</label>
             <pv-input-text id="baby-name" type="text" class="w-full" v-model="form.bebe" placeholder="Andres Cardenas Valenzuela"/>
           </div>
           <div class="col-12 md:col-6">
-            <label for="baby-birth-date" class="block text-900 font-medium mb-2">Baby's Birth Date</label>
+            <label for="baby-birth-date" class="block font-medium mb-2">Baby's Birth Date</label>
             <DatePicker id="baby-birth-date" class="w-full" v-model="form.nacimiento" placeholder="2025-05-01" />
           </div>
           <div class="col-12 md:col-6">
-            <label for="assigned-doctor" class="block text-900 font-medium mb-2">Assigned Doctor</label>
+            <label for="assigned-doctor" class="block font-medium mb-2">Assigned Doctor</label>
             <pv-select id="assigned-doctor" class="w-full" :options="doctorOptions" v-model="form.doctor" placeholder="Maria Perez"/>
           </div>
         </div>
--- src/patient-management/pages/patient-management.component.vue
+++ src/patient-management/pages/patient-management.component.vue
@@ -2,79 +2,25 @@
 import PatientListComponent from "../components/patient-list.component.vue";
 import {PatientApiService} from "../services/patient-api.service.js";
 import {PatientAssembler} from "../services/patient.assembler.js";
+import {DoctorService} from "../services/doctor.service.js";
 
 export default {
   name: "patient-management",
   components: {PatientList: PatientListComponent},
   data() {
     return {
-      patients: [
-        {
-          "id": "1",
-          "name": "Lucia Camaro Valenzuela Ramirez",
-          "email": "lcvr@hotmail.com",
-          "phone": "555-1234",
-          "baby": {
-            "id": "1111",
-            "motherId": "1",
-            "name": "Andres Cardenas Valenzuela",
-            "birthDate": "2025-05-01",
-            "gender": "Male"
-          },
-          "assignedDoctor": "Maria Perez"
-        },
-        {
-          "id": "2",
-          "name": "Maria Lopez",
-          "email": "marialopez@hotmail.com",
-          "phone": "555-5678",
-          "baby": {
-            "id": "2222",
-            "motherId": "2",
-            "name": "Romani Ramirez Lopez",
-            "birthDate": "2025-04-29",
-            "gender": "Male"
-          },
-          "assignedDoctor": "Maria Perez"
-        },
-        {
-          "id": "3",
-          "name": "Ana Machado Navarro",
-          "email": "anamachado@gmail.com",
-          "phone": "555-8765",
-          "baby": {
-            "id": "3333",
-            "motherId": "3",
-            "name": "Valentina Vasquez Machado",
-            "birthDate": "2025-04-29",
-            "gender": "Female"
-          },
-          "assignedDoctor": "Maria Perez"
-        },
-        {
-          "id": "4",
-          "name": "Sofia Alvarez Oliva",
-          "email": "sofiaalva@gmail.com",
-          "phone": "999788888",
-          "baby": {
-            "id": "4444",
-            "motherId": "4",
-            "name": "Mateo Alvarez Oliva",
-            "birthDate": "2025-04-29",
-            "gender": "Male"
-          },
-          "assignedDoctor": "Maria Perez"
-        }
-      ],
-      patientsApi: new PatientApiService()
+      patients: [],
+      patientsApi: new PatientApiService(),
+      doctorsApi: new DoctorService(),
+      doctorId: 1
     }
   },
   methods: {
-    getPatients() {
+    getPatientsByActiveDoctorId(doctorId) {
       this.patientsApi.getAll()
           .then((response) => {
             console.log(response);
-            //this.patients = PatientAssembler.toEntitiesFromResponse(response);
+            this.patients = PatientAssembler.toEntitiesFromResponse(response);
           })
           .catch((error) => {
             this.errors.push(error);
@@ -82,7 +28,7 @@ export default {
     }
   },
   created() {
-    this.getPatients();
+    this.getPatientsByActiveDoctorId(this.doctorId);
   }
 }
 </script>
